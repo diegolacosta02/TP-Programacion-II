c2e7f2f3f1c5a8a90330b5b268bb58ef063b092a
GONZALOFLEITA <135912246+GONZALOFLEITA@users.noreply.github.com>
2025-09-13 23:59:14 -0300

    Se agregan calculos sobre clase SUV

--- src/compacto.ts
+++ src/compacto.ts
@@ -4,20 +4,31 @@ import Vehiculo from "./Vehiculo";
 export default class Compacto extends Vehiculo{
     protected tarifaBase: number;
     protected cargoAdicional: number;
+    protected kmPermitido:number
     
     constructor(){
         super()
         this.tarifaBase = 30
         this.cargoAdicional = 0.15
+        this.kmPermitido = 100
     }
 
+    public getTarifaBase():number{
+        return this.tarifaBase
+    }
+    public getCargoAdicional():number{
+        return this.cargoAdicional
+    }
+    public getKmPermitido():number{
+        return this.kmPermitido
+    }
     public calcularTarifa(reserva:Reserva): number {
-        const tarifaBase = 30 * reserva.getDiasReservados()
-        const kmPermitidos = 100 * reserva.getDiasReservados()
+        const tarifaBase = this.getTarifaBase() * reserva.getDiasReservados()
+        const kmPermitidos = this.getKmPermitido() * reserva.getDiasReservados()
         let extra = 0
 
         if (reserva.getKmRecorridos() > kmPermitidos) {
-            extra = (reserva.getKmRecorridos() - kmPermitidos) * 0.15
+            extra = (reserva.getKmRecorridos() - kmPermitidos) * this.getCargoAdicional()
         }
 
         return tarifaBase + extra
