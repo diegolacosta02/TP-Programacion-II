0c6d305780e4737523fa046c2e78e1f63c16122c
FacundoManfredi-00 <facundomanfredi06@gmail.com>
2025-09-13 22:38:03 -0300

    Se crea clase Reseva

---
+++ src/reserva.ts
@@ -0,0 +1,10 @@
+export default class Reserva{
+    private cliente: string;
+    private fechaInicio: number;
+    private fechaFin: number;
+
+
+    public ReservaVehiculo(): number{
+        
+    }
+}
