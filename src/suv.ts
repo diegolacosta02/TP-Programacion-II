aa0440451c34cc28ef62e35285a1af89836bfed3
FacundoManfredi-00 <facundomanfredi06@gmail.com>
2025-09-16 11:17:40 -0300

    se realiza el calculo de tarifa en el Suv

--- src/suv.ts
+++ src/suv.ts
@@ -3,16 +3,34 @@ import Vehiculo from "./Vehiculo";
 
 export default class SUV extends Vehiculo{
     protected cargoFijoAdicional: number
+    protected tarifaBase:number
+    protected cargoAdicional:number
+
     constructor(){
         super()
         this.tarifaBase = 80
         this.cargoFijoAdicional = 15
         this.cargoAdicional = 0.25
     }
+public getTarifaBase():number{
+    return this.tarifaBase
+}
+
+public getCargoFijoAdicional():number{
+    return this.cargoFijoAdicional
+}
+
+public getCargoAdicional():number{
+    return this.cargoAdicional
+}
 
     public calcularTarifa(reserva:Reserva): number {
-        let resultado: number = 0
-        
-        return resultado
+        const tarifaBase = this.getTarifaBase() * reserva.getDiasReservados()
+        let kmPermitidos=0;
+        if(reserva.getKmRecorridos()>500){
+         kmPermitidos=reserva.getKmRecorridos()
+        }
+        const extra = this.getCargoFijoAdicional()*reserva.getDiasReservados()+ kmPermitidos*this.cargoAdicional()
+        return tarifaBase*extra
     }
 }
