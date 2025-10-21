92afebfeb971084b5f62b77c436de2c7a5bbf50d
FacundoManfredi-00 <facundomanfredi06@gmail.com>
2025-10-21 17:23:48 -0300

    actualizo el diagrama de clases, y corrijo la logica de calculo en clase SUV y Sedan

--- src/compacto.ts
+++ src/compacto.ts
@@ -5,20 +5,23 @@ export default class Compacto extends Vehiculo {
     private tarifaBase: number;
     private cargoAdicional: number;
     private kmPermitidos:number;
-    
+
     constructor(matricula:number){
         super(matricula)
         this.tarifaBase = 30
         this.cargoAdicional = 0.15
         this.kmPermitidos = 100
     }
-    
+public getCargoAdicional ():number{
+    return this.cargoAdicional
+
+}
     public calcularTarifa(reserva:Reserva): number {
         const tarifaBase = this.tarifaBase * reserva.getDiasReservados()
         const kmPermitidos = this.kmPermitidos * reserva.getDiasReservados()
         let extra = 0
         if (reserva.getKmRecorridos() > kmPermitidos) {
-            extra = (reserva.getKmRecorridos() - kmPermitidos) * this.cargoAdicional
+            extra = reserva.getKmRecorridos()  * this.getCargoAdicional();
         }
         const tarifaTemporada = this.gestorTemporadas.getPorcentajeTemporada(reserva) * tarifaBase / 100;
 
--- src/sedan.ts
+++ src/sedan.ts
@@ -1,21 +1,34 @@
 import Vehiculo from "./vehiculo";
 import Reserva from "./reserva";
 
-export default class Sedan extends Vehiculo {
+export default class SUV extends Vehiculo {
 
-    private tarifaBase:number
+    private tarifaBase: number
     private cargoAdicional: number
+    private cargoFijoSeguro: number
+    private kmPermitidos:number
 
     constructor(matricula:number){
         super(matricula)
-        this.tarifaBase = 50
-        this.cargoAdicional = 0.20
+        this.tarifaBase = 80
+        this.cargoFijoSeguro = 15
+        this.cargoAdicional = 0.25
+        this.kmPermitidos = 500
     }
-    public calcularTarifa(reserva: Reserva): number {
-        const tarifaBase= this.tarifaBase * reserva.getDiasReservados()
-        const extra = this.cargoAdicional * reserva.getKmRecorridos()
-        const tarifaTemporada = this.gestorTemporadas.getPorcentajeTemporada(reserva) * tarifaBase / 100;         
-       
-        return tarifaBase + extra + tarifaTemporada;
+
+    public getCargoAdicional():number{
+        return this.cargoAdicional;
+    }
+
+    public calcularTarifa(reserva:Reserva): number {
+        const tarifaBase = this.tarifaBase * reserva.getDiasReservados()
+        const cargoSeguro = this.cargoFijoSeguro * reserva.getDiasReservados()
+        let extra = 0
+        if (reserva.getKmRecorridos() > this.kmPermitidos) {
+            extra = reserva.getKmRecorridos() * this.getCargoAdicional();
+        }
+        const tarifaTemporada = this.gestorTemporadas.getPorcentajeTemporada(reserva) * tarifaBase / 100;
+
+        return tarifaBase + cargoSeguro + extra + tarifaTemporada;
     }
 }
